18a129990d22a5d8277105341ff3aef8a5f2ad8b
NBXOFF <nbx-off@outlook.fr>
2026-05-19 11:24:19 +0200

    add prettyPrint function

--- bst.js
+++ bst.js
@@ -22,3 +22,14 @@ function buildTree(array) {
 }
 
 buildTree(arr);
+
+// Print the tree in a structured format
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null || node === undefined) {
+    return;
+  }
+
+  prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+};
